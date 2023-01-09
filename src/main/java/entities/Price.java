b105f535988556b5efbb5aa91e0aa76c3e58f11e
İlker Şelimen <ilkerselimen@gmail.com>
2023-01-09 21:33:56 +0300

    Initial commit

---
+++ src/main/java/entities/Price.java
@@ -0,0 +1,13 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class Price {
+
+
+
+
+}
---
+++ src/main/java/entities/SeaFood.java
@@ -0,0 +1,23 @@
+package entities;
+
+import java.util.Date;
+
+public class SeaFood {
+
+    // int productId;
+
+    String product;
+
+    double price;
+
+    double lastPrice;
+
+    double unitIsStock;
+
+    //Date arrivalDate; // arrival date - urun gelis tarihi
+
+    Date mfgDate; // manufacturing date - imalat tarihi
+
+    Date expDate; // expire date - son kullanma tarihi --> long
+
+}
